5703be7dbb8d783b5072e5ead75e061fd7355ec1
Jake Edwards <edwardsjake@live.com>
2021-02-20 16:27:18 -0600

    Add Color Helper to generate colors

--- modern_react_bootcamp/Color-Project/colors-app/src/App.js
+++ modern_react_bootcamp/Color-Project/colors-app/src/App.js
@@ -1,11 +1,13 @@
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 import './App.css';
 
 function App() {
+  console.log(generatePalette(seedColors[4]));
   return (
     <div className="">
-      <Palette {...seedColors[6]}/>
+      <Palette {...seedColors[4]}/>
     </div>
   );
 }
