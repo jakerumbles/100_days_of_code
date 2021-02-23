da881f99325966bbaa837074600a89d639cc6062
Jake Edwards <edwardsjake@live.com>
2021-02-22 20:53:15 -0600

    Fix Palette Route

--- modern_react_bootcamp/Color-Project/colors-app/src/App.js
+++ modern_react_bootcamp/Color-Project/colors-app/src/App.js
@@ -5,12 +5,24 @@ import { generatePalette } from './colorHelpers';
 import './App.css';
 
 function App() {
-  console.log(generatePalette(seedColors[4]));
+  const findPalette = (id) => {
+    return seedColors.find(function(palette) {
+      return palette.id === id
+    });
+  };
+
   return (
     <div>
       <Switch>
         <Route exact path='/' render={() => <h1>PALETTE LIST GOES HERE</h1>} />
-        <Route exact path='/palette/:id' render={() => <h1>INDIVIDUAL PALETTE</h1>} />
+        <Route 
+          exact 
+          path='/palette/:id' 
+          render={routeProps => (
+            <Palette palette={generatePalette(findPalette(routeProps.match.params.id))
+            } />
+          )} 
+        />
       </Switch>
       {/* <div>
         <Palette palette={generatePalette(seedColors[4])}/>
