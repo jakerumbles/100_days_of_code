3bf0b895acfc746ad329dd49d379eafa3551ef12
Jake Edwards <edwardsjake@live.com>
2021-02-22 20:31:55 -0600

    Add react router

--- modern_react_bootcamp/Color-Project/colors-app/src/App.js
+++ modern_react_bootcamp/Color-Project/colors-app/src/App.js
@@ -1,4 +1,5 @@
 import Palette from './Palette';
+import { Route, Switch } from 'react-router-dom';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 import './App.css';
@@ -6,8 +7,14 @@ import './App.css';
 function App() {
   console.log(generatePalette(seedColors[4]));
   return (
-    <div className="">
-      <Palette palette={generatePalette(seedColors[4])}/>
+    <div>
+      <Switch>
+        <Route exact path='/' render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+        <Route exact path='/palette/:id' render={() => <h1>INDIVIDUAL PALETTE</h1>} />
+      </Switch>
+      {/* <div>
+        <Palette palette={generatePalette(seedColors[4])}/>
+      </div> */}
     </div>
   );
 }
