5db73d2f9a9d555716a1c3ce3b3cc8f65457cfed
Jake Edwards <edwardsjake@live.com>
2022-02-04 16:16:08 -0600

    Display colors in SingleColorPalette

--- modern_react_bootcamp/Color-Project/colors-app/src/App.js
+++ modern_react_bootcamp/Color-Project/colors-app/src/App.js
@@ -1,37 +1,49 @@
-import Palette from './Palette';
-import PaletteList from './PaletteList';
-import { Route, Switch } from 'react-router-dom';
-import seedColors from './seedColors';
-import { generatePalette } from './colorHelpers';
-import SingleColorPalette from './SingleColorPalette';
-import './App.css';
+import Palette from "./Palette";
+import PaletteList from "./PaletteList";
+import { Route, Switch } from "react-router-dom";
+import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
+import SingleColorPalette from "./SingleColorPalette";
+import "./App.css";
 
 function App() {
   const findPalette = (id) => {
-    return seedColors.find(function(palette) {
-      return palette.id === id
+    return seedColors.find(function (palette) {
+      return palette.id === id;
     });
   };
 
   return (
     <div>
       <Switch>
-        <Route 
-          exact 
-          path='/' 
-          render={routeProps => <PaletteList palettes={seedColors} {...routeProps} />} />
-        <Route 
-          exact 
-          path='/palette/:id' 
-          render={routeProps => (
-            <Palette palette={generatePalette(findPalette(routeProps.match.params.id))
-            } />
-          )} 
-        />
-        <Route 
+        <Route
           exact
-          path='/palette/:paletteId/:colorId' 
-          render={() => <SingleColorPalette />} />
+          path="/"
+          render={(routeProps) => (
+            <PaletteList palettes={seedColors} {...routeProps} />
+          )}
+        />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(findPalette(routeProps.match.params.id))}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/palette/:paletteId/:colorId"
+          render={(routeProps) => (
+            <SingleColorPalette
+              colorId={routeProps.match.params.colorId}
+              palette={generatePalette(
+                findPalette(routeProps.match.params.paletteId)
+              )}
+            />
+          )}
+        />
       </Switch>
       {/* <div>
         <Palette palette={generatePalette(seedColors[4])}/>
--- modern_react_bootcamp/Color-Project/colors-app/src/Palette.js
+++ modern_react_bootcamp/Color-Project/colors-app/src/Palette.js
@@ -1,52 +1,51 @@
-import React, { Component } from 'react'
-import ColorBox from './ColorBox';
-import Navbar from './Navbar';
-import './Palette.css';
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import Navbar from "./Navbar";
+import "./Palette.css";
 
 class Palette extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { level: 500, format: "hex" };
-		this.changeLevel = this.changeLevel.bind(this);
-		this.changeFormat = this.changeFormat.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = { level: 500, format: "hex" };
+    this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
+  }
 
-	changeLevel(level) {
-		this.setState({ level });
-	}
+  changeLevel(level) {
+    this.setState({ level });
+  }
 
-	changeFormat(val) {
-		this.setState({ format: val });
-	}
+  changeFormat(val) {
+    this.setState({ format: val });
+  }
 
-	render() {
-		const { colors, paletteName, emoji, id } = this.props.palette;
-		const { level, format } = this.state;
-		const colorBoxes = colors[level].map(color => (
-			<ColorBox 
-				background={color[format]} 
-				name={color.name} 
-				key={color.id}
-				moreUrl={`/palette/${id}/${color.id}`}
-			/>
-		))
-		return (
-			<div className="Palette">
-				<Navbar 
-					level={level} 
-					changeLevel={this.changeLevel} 
-					handleChange={this.changeFormat}	
-				/>
-				<div className='Palette-colors'>
-					{colorBoxes}
-				</div>
-				<footer className='Palette-footer'>
-					{paletteName.id}
-					<span className='emoji'>{emoji}</span>
-				</footer>
-			</div>
-		)
-	}
+  render() {
+    const { colors, paletteName, emoji, id } = this.props.palette;
+    const { level, format } = this.state;
+    const colorBoxes = colors[level].map((color) => (
+      <ColorBox
+        background={color[format]}
+        name={color.name}
+        key={color.id}
+        moreUrl={`/palette/${id}/${color.id}`}
+        showLink={true}
+      />
+    ));
+    return (
+      <div className="Palette">
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        />
+        <div className="Palette-colors">{colorBoxes}</div>
+        <footer className="Palette-footer">
+          {paletteName.id}
+          <span className="emoji">{emoji}</span>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default Palette;
